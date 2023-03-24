606b4e98b9684e735d16f6496d366f02ab729349
BintangDanendra <bintang.danendra140704@gmail.com>
2023-03-24 16:46:53 +0700

    first commite

--- insertion.sort/insertion.sort.cpp
+++ insertion.sort/insertion.sort.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 int arr[20];                                    // Membuat array dengan panjang data 20
-int n;                                          // Membuat variabel input n
+int n;                                          // Membuat variabel input n 
 
 void input()
 {    // Procedure Input
